accf1bfbb778e543d4c2c011740cb09a758578ee
Dulc3L3on <ldulce2001@gmail.com>
2020-04-18 08:17:02 -0600

    DulceLeon 201930139...agregue los algoritmos de ordenamiento por burbuja y seleccion, adaptados para un tipo String; de igual manera se agregan los métods de búsqueda secuencial y binaria basados en el nombre del jugador

--- TorneoTennis/src/main/java/torneotennis/Main.java
+++ TorneoTennis/src/main/java/torneotennis/Main.java
@@ -12,6 +12,9 @@ public class Main {
     public static void main(String[] args) {
         Torneo torneo = new Torneo();
         torneo.agregarDatos();
+	torneo.escogerMetodoOrdenamiento();
+        torneo.escogerMetodoOrdenamiento();
+        torneo.buscarJugadorPorNombre();
     }
 
 }
--- TorneoTennis/src/main/java/torneotennis/manejador/controlJugadores.java
+++ TorneoTennis/src/main/java/torneotennis/manejador/controlJugadores.java
@@ -60,7 +60,7 @@ public class controlJugadores {
      * @return
      * @throws java.lang.Exception
      */
-    public Jugador buscarJugadorBinario(Jugador[] jugadores, int punteo) throws Exception {
+    public Jugador buscarJugadorBinarioPorPunteo(Jugador[] jugadores, int punteo) throws Exception {
         //Lo utilizamos para mostrar los ciclos realizados
         int ciclos = 0;
 
@@ -81,6 +81,102 @@ public class controlJugadores {
         } while (pInicio != pFinal || pInicio > pFinal);
         throw new Exception("No se ha encontrado al jugador");
     }
+	
+ /**
+     *TIpo de búsqueda binaria de nombres
+     * @param jugadores
+     * @param nombreJugador
+     * @return
+     */
+    public Jugador buscarBinariamenteJugadorPorNombre(Jugador[] jugadores, String nombreJugador){//sería útil implementar en este método el ingresar el punteo por si acaso los jugadores tuvieran el mismo nombre de pila
+            int limiteIzquierdo=0;
+            int  limiteDerecho=jugadores.length-1;
+            int centroArreglo;            
+                        
+            while(limiteIzquierdo<=limiteDerecho){
+                 centroArreglo=(limiteIzquierdo+limiteDerecho)/2;
+                 
+                 if(jugadores[centroArreglo]==null){
+                     System.out.println("NO se ha encontrado al jugador");
+                     return null;
+                 }                 
+                 if(jugadores[centroArreglo].getNombre().compareToIgnoreCase(nombreJugador)==0){
+                          return jugadores[centroArreglo];
+                 }
+                 else if(jugadores[centroArreglo].getNombre().compareToIgnoreCase(nombreJugador)<0){
+                          limiteDerecho=centroArreglo-1;  
+                 }
+                 
+                 else{
+                        limiteIzquierdo=centroArreglo+1;
+                 }                                                   
+            }
+            
+            return null;
+        }//creo que para los métodos de ordenamiento por nombre solo deberé implementar a comparte to en lugar de los ifguales, puesto que el algoritmo ya se encarga de recorrer el arreglo de tal forma que quede todo ordenado
+    
+        public Jugador buscarSecuencialmentePorNombre(Jugador[] jugadores, String nombreJugador){
+                for(int numeroJUgadorActual=0; numeroJUgadorActual<jugadores.length; numeroJUgadorActual++){
+                           if(nombreJugador.compareTo(jugadores[numeroJUgadorActual].getNombre())==0) {
+                                    return jugadores[numeroJUgadorActual];
+                           }
+                }
+                
+                System.out.println("\nNo se encontro a un jugador con ese nombre");
+                return null;
+        }
+        
+        public Jugador[] bubbleSort(Jugador[] jugadores){
+            Jugador jugadorTemporal;
+            
+            for(int numeroVecesRepetido=0; numeroVecesRepetido<jugadores.length-1; numeroVecesRepetido++){
+                    for(int numeroPosicionActual=0; numeroPosicionActual<jugadores.length-1; numeroPosicionActual++){
+                           if(jugadores[numeroPosicionActual].getNombre().compareToIgnoreCase(jugadores[numeroPosicionActual+1].getNombre())>0) {
+                                    jugadorTemporal=jugadores[numeroPosicionActual];
+                                    jugadores[numeroPosicionActual]=jugadores[numeroPosicionActual+1];
+                                    jugadores[numeroPosicionActual+1]=jugadorTemporal;
+                           }
+                    }
+                
+            }
+            
+            return jugadores;
+        
+        }
+        
+    /**
+     *Corrobora por cada vuelta que el índice que posea sea el correspondiente al valor mínimo según los datos 
+     * del arreglo.
+     * @param jugadores
+     * @return
+     */
+    public Jugador[] selectionSort(Jugador[] jugadores){
+               Jugador jugadorTemporal;
+               
+               for (int lugarDelMInimoActual = 0; lugarDelMInimoActual < jugadores.length-1; lugarDelMInimoActual++) {//no es <tamArreglo, pues el último DEBE ser el > puesto que nunca se agarró
+                   int indiceMInimo=lugarDelMInimoActual;
+                   
+                        for (int lugarARevisar = lugarDelMInimoActual+1; lugarARevisar < jugadores.length; lugarARevisar++) {
+                                if(jugadores[lugarARevisar].getNombre().compareToIgnoreCase(jugadores[indiceMInimo].getNombre())<0){
+                                           indiceMInimo=lugarARevisar ;
+                                }                                                                                    
+                   }//fin del for que se de actualizar el índice al correspondiente del dato menor
+                        
+                        jugadorTemporal=jugadores[lugarDelMInimoActual];
+                        jugadores[lugarDelMInimoActual]=jugadores[indiceMInimo];
+                        jugadores[indiceMInimo]=jugadorTemporal;                
+                }               
+               return jugadores;
+        }
+    
+     //resulta eficaz puesto que sin importar que valor haya sido seleccionado como referencia
+     // cuando recorres a todos los elementos involucrados, lograrás encontrar al valor más pequeño de todos ellos
+     // o simplemente terminarás de confirmar que el que tenías era más pequeño de todos, por lo tanto, ya no se 
+     // requiere iniciar la vuelta desde la 1ra posición puesto que ya te aseguraste de haber agarrado el menor de todos
+     //lo cual te asegura que al seguir adelante solo tendrás valores iguales o mayores al más peq que encontraste en 
+     // dicha vuelta.. para de último sustituir el valor que se hallaba en dicho índice y rellenar ese espacio donde se encontraba
+    //con el valor que estaba en esa ubicación la cual no le correspondía, esto no genera problemas pues después el mismo método se
+    //encargará de llevarlo a su lugar correspondiente si es que no estaba en el correcto	
 
     /**
      * Se devuelte una arreglo con el tamaño redimensionado
